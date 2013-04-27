22c2e272d933cfa0292df486c56bdf00730110b6
Patrick Pettegrow <thormot36@gmail.com>
2013-04-26 22:22:10 -0400

    Elemental Reference

---
+++ common/chemcraft/items/ChemcraftElementalItems.java
@@ -0,0 +1,18 @@
+package chemcraft.items;
+
+import net.minecraft.item.Item;
+
+public class ChemcraftElementalItems {
+    
+    public static final Item elementalhydrogen;
+    public static final Item elementalhelium;
+    public static final Item elementallithium;
+    public static final Item elementalberyllium;
+    public static final Item elementalboron;
+    public static final Item elementalcarbon;
+    public static final Item elementalnitrogen;
+    public static final Item elementaloxygen;
+    public static final Item elementalfluorine;
+    public static final Item elementalneon;
+
+}
